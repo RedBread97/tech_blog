80e289c744272097b599a8875bae0fb72914ac0d
Alex Lane <97alane@gmail.com>
2022-05-09 19:30:28 -0500

    added post model

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const routes = require('./controllers');
 const exphbs = require('express-handlebars');
-const sequelize = require('./connections');
+const sequelize = require('./config/connections');
 
 const PORT = process.env.PORT || 3001;
 
